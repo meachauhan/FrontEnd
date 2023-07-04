6ddcba546f38b4897d8d6e4e8f1dda6cbc9cf576
Akash Chauhan <meachauhan@gmail.com>
2023-07-04 17:17:56 +0530

    Made Chaneges in Responsive Popup menu

--- RessponsiveNavbar/script.js
+++ RessponsiveNavbar/script.js
@@ -1,7 +1,15 @@
 let menu=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
-
+var count=0;
 menu.onclick=()=>{
-    // menu.classList.toggle('bx-x');
-    menu.classList.toggle('open');
+   
+    if(count%2==0){
+        menu.classList.toggle('bx-x');
+        navbar.classList.add("open");
+        count++;
+    }else{
+        menu.classList.remove("bx-x");
+        navbar.classList.remove("open");
+        count++;
+    }
 }
