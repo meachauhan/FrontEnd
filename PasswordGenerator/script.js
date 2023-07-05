11fb1b72ab6a9a2b96c06cf17a78f1f070d9ede8
Akash Chauhan <meachauhan@gmail.com>
2023-07-05 15:33:33 +0530

    Added Password Length Funtctionality

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -1,4 +1,5 @@
 var password=document.getElementById("password");
+var len=document.getElementById("length");
 
 function copyPassword() {
     var copyText = document.getElementById("password");
@@ -9,9 +10,9 @@ function copyPassword() {
 
   function genPassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 12;
+    var passwordLength = len.value;
     var password = "";
- for (var i = 0; i <= passwordLength; i++) {
+ for (var i = 0; i < passwordLength; i++) {
    var randomNumber = Math.floor(Math.random() * chars.length);
    password += chars.substring(randomNumber, randomNumber +1);
   }
