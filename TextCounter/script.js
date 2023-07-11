f52f1e14e8ca11a5f8f4692d4c82d3dd281befde
Akash Chauhan <meachauhan@gmail.com>
2023-07-11 21:30:10 +0530

    Implemented Vowel and Consonent Features

--- TextCounter/script.js
+++ TextCounter/script.js
@@ -1,8 +1,19 @@
 const userText = document.getElementById('inputText')
 const characters = document.getElementById('char')
 const totalWords = document.getElementById('words')
+const vowels = document.getElementById('vowel')
+const consonents = document.getElementById('consonents')
 
 userText.addEventListener('input', function () {
+    
     characters.innerHTML = userText.value.length + ' Characters';
     totalWords.innerHTML = userText.value.trim().split(' ').length + ' Words'
+    vowels.innerHTML=countVowels(userText.value)+ ' Vowels'
+    consonents.innerHTML=userText.value.length-countVowels(userText.value)  +' Consonents'
+
 })
+function countVowels(s) {
+    
+    var c = s.match(/[aeiou]/gi);
+    return c === null ? 0 : c.length;
+  }
